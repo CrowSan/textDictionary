0039d0ba8d5ae66205053b9eab4b0728b8b12690
farhad <farhadfarzi.oo@gmail.com>
2024-08-03 17:51:11 +0330

    starting renewing app - step 0

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include_once "autoloader.php"?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NovelEast text toolBox</title>
+</head>
+<body>
+    
+</body>
+</html>
