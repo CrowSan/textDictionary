cf8960c774ecd8126d75d3f3bf67ea66559f04c4
farhad <farhadfarzi.oo@gmail.com>
2024-08-06 16:21:21 +0330

    validator function in helper.php

--- process.php
+++ process.php
@@ -1,4 +1,5 @@
 <?php
+include "utilities/helper.php";
 
 #validate method
 if (!$_SERVER['REQUEST_METHOD'] == 'post' ) {
@@ -7,19 +8,7 @@ if (!$_SERVER['REQUEST_METHOD'] == 'post' ) {
 }
 
 if (array_key_exists('nameToNum', $_POST)) {
-    #validate file extention
-    if (!strpos($_FILES['textFile']['name'], '.txt') && !strpos($_FILES['jsonFile']['name'], '.json')) {
-        echo "wrong format";
-        return false;
-    }
-
-    #validate json file
-    $jsonFile = file_get_contents($_FILES['jsonFile']['tmp_name']);
-    if (json_decode($jsonFile) === null || json_last_error() !== JSON_ERROR_NONE ) {
-        echo "your Json file is not right or have internal error";
-        return false;
-    } 
-
+    if (validator($_FILES['textFile'],$_FILES['jsonFile'])) {
     $textFile = $_FILES['textFile']['tmp_name'];
     $jsonFile = $_FILES['jsonFile']['tmp_name'];
     $textName = $_FILES['textFile']['name'];
@@ -27,31 +16,22 @@ if (array_key_exists('nameToNum', $_POST)) {
     include_once "app/text/textNumberer.php";
     $numCls = new \app\text\textNumberer($textFile, $jsonFile, $textName);
     $numCls -> numberText();
-    echo "numToName";
+    }
+    echo "nameToNum Validation failed";
 
 }
 
 if (array_key_exists('numToName', $_POST)) {
-    #validate file extention
-    if (!strpos($_FILES['textFile']['name'], '.txt') && !strpos($_FILES['jsonFile']['name'], '.json')) {
-        echo "wrong format";
-        return false;
-    }
-
-    #validate json file
-    $jsonFile = file_get_contents($_FILES['jsonFile']['tmp_name']);
-    if (json_decode($jsonFile) === null || json_last_error() !== JSON_ERROR_NONE ) {
-        echo "your Json file is not right or have internal error";
-        return false;
-    } 
-
+  if (validator($_FILES['textFile'],$_FILES['jsonFile'])) {
     $textFile = $_FILES['textFile']['tmp_name'];
     $jsonFile = $_FILES['jsonFile']['tmp_name'];
     $textName = $_FILES['textFile']['name'];
+
     include_once "app/text/namedText.php";
     $transformer = new TextTransformer($jsonFile, $textFile, $textName);
     $transformer->transformText();
-    echo "numToName";
+  }
+  echo "numToName Validation failed";
 }
 
 if (array_key_exists('fileMulti', $_POST)) {
--- utilities/helper.php
+++ utilities/helper.php
@@ -1,2 +1,18 @@
-#error massages
-#and more
+<?php
+
+function validator($textFile, $jsonFile){
+    #validate file extention
+    if (!strpos($textFile['name'], '.txt') && !strpos($jsonFile['name'], '.json')) {
+        echo "wrong format! <br>";
+        return false;
+    }
+
+    #validate json file
+    $jsonFile = file_get_contents($jsonFile['tmp_name']);
+    if (json_decode($jsonFile) === null || json_last_error() !== JSON_ERROR_NONE ) {
+        echo "your Json file is not right or have internal error!<br>";
+        return false;
+    } 
+
+    return true;
+}
