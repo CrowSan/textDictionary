fd5230727accfef1f2db8a2315bcdffca8ef8425
farhad <farhadfarzi.oo@gmail.com>
2024-08-06 22:38:52 +0330

    fileMulti first codes

--- app/file/fileMultiplier.php
+++ app/file/fileMultiplier.php
@@ -1,16 +1,31 @@
 <?php
 
 // "Chapter 101((.|\n)*)chapter 105"
-$file = "";
-$firstChapter = "";
-$finalChapter = "";
-$finalFile = "";
 
-$n1 = 101;
+class fileMultiplier{
+    private $text;
+    private $start;
+    private $end;
+    
+    
+    public function __construct($text, $start, $end)
+    {
+        $this->text = file_get_contents($text);
+        $this->start = $start;
+        $this->end = $end;
+    }
 
-while ($n1 <= 150) {
-    echo $n1 . " - n1<br>";
-    $n2 = $n1 + 5;
-    echo $n2 . " - n2<br>";
-    $n1 +=5;
+    private function seperator(){
+        while ($this->start <= $this->end) {
+            echo $this->start . " - n1<br>";
+            $n2 = $this->start + 5;
+            echo $n2 . " - n2<br>";
+            $this->start +=5;
+        }
+
+    }
+
 }
+
+
+
--- autoloader.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-// spl_autoload_register();
